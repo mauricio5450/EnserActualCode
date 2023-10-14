98236794c65594180b77997a15f4bf0ce51c102e
mauricio5450 <mauricio.rodriguez5450@gmail.com>
2023-10-14 07:36:09 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" >
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <label>
+            <div class="slide">
+                <h1 class="menu-h1">MENU</h1>
+                <ul>
+                    <li><a href="#"><i class="fa fa-tv"></i>Dashboard</a></li>
+                    <li><a href="#"><i class="fa fa-folder"></i>Files</a></li>
+                    <li><a href="#"><i class="fa fa-calendar"></i>Calender</a></li>
+                    <li><a href="#"><i class="fa fa-cogs"></i>Settings</a></li>
+                </ul>
+            </div>
+        </label>
+    </body>
+</html>
