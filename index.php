a1762e2cfc6e9e79d873e6777a5c0c755b27377a
mauricio5450 <mauricio.rodriguez5450@gmail.com>
2023-10-14 17:28:15 -0800

    NewPlan

--- index.php
+++ index.php
@@ -1,20 +1,16 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" >
+        <meta character="utf-8">
+        <meta name="viewpoint" content="width=device-width,initial-scale=1.0">
+        <title>Dashboard</title>
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <label>
-            <div class="slide">
-                <h1 class="menu-h1">MENU</h1>
-                <ul>
-                    <li><a href="#"><i class="fa fa-tv"></i>Dashboard</a></li>
-                    <li><a href="#"><i class="fa fa-folder"></i>Files</a></li>
-                    <li><a href="#"><i class="fa fa-calendar"></i>Calender</a></li>
-                    <li><a href="#"><i class="fa fa-cogs"></i>Settings</a></li>
-                </ul>
-            </div>
-        </label>
+        <div class="grid-container">
+            <header class="header"></header>
+            <aside id="sidebar"></aside>
+            <main class = "main-container"></main>
+        </div>
     </body>
 </html>
