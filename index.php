011f127a35ab557115bfc10cceb6e1e81a3cef80
mauricio5450 <mauricio.rodriguez5450@gmail.com>
2023-10-16 22:24:01 -0700

    ActualCode

--- index.php
+++ index.php
@@ -3,23 +3,87 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet"/>
+        <link rel="stylesheet" href="styles/style.css">
         <title>Dashboard</title>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-        <link rel="stylesheet" href="style.css">
     </head>
+
     <body>
-        <div class="grid-container">
-            <header class="header">
-                <div class="menu-icon">
-                    
+        <div class="container">
+            <aside>
+                <div class="toggle">
+                    <div class="logo">
+                        <img src="images/Temps.png">
+                        <h2>Enser</h2>
+                    </div>
+                    <div class="close" id="close-btn">
+                        <span class="material-symbols-outlined">
+                            close
+                        </span>
+                    </div>
                 </div>
-            </header>
-            <aside id="sidebar">
-                <div class="sidebar-title">
-                    <span class="material-icons-outlined">select_check_box</span>Inventory
+                <!--This is the beggining of the sidebar-->
+                <div>
+                    <div class="sidebar">
+                        <a href="#">
+                        <span class="material-symbols-outlined">
+                            dashboard
+                        </span>
+                        <h3>Dashboard</h3>
+                        </a>
+                        <a href="#">
+                            <span class="material-symbols-outlined">
+                                person
+                            </span>
+                            <h3>Students</h3> 
+                        </a>
+                        <a href="#">
+                            <span class="material-symbols-outlined">
+                                school
+                            </span>
+                            <h3>Courses</h3> 
+                        </a>
+                        <a href="#">
+                            <span class="material-symbols-outlined">
+                                inventory
+                            </span>
+                            <h3>Current Courses</h3> 
+                        </a>
+                        <a href="#">
+                            <span class="material-symbols-outlined">
+                                deployed_code
+                            </span>
+                            <h3>Projects</h3> 
+                        </a>
+                        <a href="#">
+                            <span class="material-symbols-outlined">
+                                design_services
+                            </span>
+                            <h3>Instructors</h3> 
+                        </a>
+                        <a href="#">
+                            <span class="material-symbols-outlined">
+                                volunteer_activism
+                            </span>
+                            <h3>Stakeholders</h3> 
+                        </a>
+                        <a href="#">
+                            <span class="material-symbols-outlined">
+                                history
+                            </span>
+                            <h3>Past Courses</h3> 
+                        </a>
+                        <a href="#">
+                            <span class="material-symbols-outlined">
+                                logout
+                            </span>
+                            <h3>Logout</h3> 
+                        </a>
+                    </div>
                 </div>
+                    <!--This is the end of the sidebar-->
             </aside>
-            <main class="main-container"></main>
         </div>
     </body>
 </html>
