94f19a2b752d60c0a47dbb4ca6ffd440f02b12e1
mauricio5450 <mauricio.rodriguez5450@gmail.com>
2023-10-16 20:28:36 -0800

    edits

--- index.php
+++ index.php
@@ -1,16 +1,25 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta character="utf-8">
-        <meta name="viewpoint" content="width=device-width,initial-scale=1.0">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Dashboard</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
         <div class="grid-container">
-            <header class="header"></header>
-            <aside id="sidebar"></aside>
-            <main class = "main-container"></main>
+            <header class="header">
+                <div class="menu-icon">
+                    
+                </div>
+            </header>
+            <aside id="sidebar">
+                <div class="sidebar-title">
+                    <span class="material-icons-outlined">select_check_box</span>Inventory
+                </div>
+            </aside>
+            <main class="main-container"></main>
         </div>
     </body>
 </html>
