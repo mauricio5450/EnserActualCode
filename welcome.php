7154df0001c388caf6e241c8b95c9e153fe244e0
mauricio5450 <mauricio.rodriguez5450@gmail.com>
2023-10-17 16:16:35 -0800

    welcome

---
+++ welcome.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet"/>
+        <link rel="stylesheet" href="styles/style.css">
+        <title>Dashboard</title>
+    </head>
+
+    <body>
+        <div class="container">
+            <aside>
+                <div class="toggle">
+                    <div class="logo">
+                        <img src="images/Temps.png">
+                        <h2>Enser</h2>
+                    </div>
+                    <div class="close" id="close-btn">
+                        <span class="material-symbols-outlined">
+                            close
+                        </span>
+                    </div>
+                </div>
+            </aside>
+        </div>
+        <main>
+            <h1>This is a test</h1>
+        </main>
+</html>
